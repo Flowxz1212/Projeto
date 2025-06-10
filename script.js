2d1d30e570afbe05f816b7beb6b52142c22169de
Miguel Santos <migulsantos28u@gmail.com>
2025-06-10 17:37:29 -0300

    Transforming to Miguel informations

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 function toggleMode() {
+  const html = document.documentElement;
 
-    const html = document.documentElement
-    
-        html.classList.toggle('light')
+  html.classList.toggle("light");
 
-        //pegar a tag img
+  //pegar a tag img
 
-        const img = document.querySelector('#profile img')
+  const img = document.querySelector("#profile img");
 
-        //substituir imagem
-          if(html.classList.contains('light'))
-        {
-            img.setAttribute('src', './assets/avatar-light.png')
-            img.setAttribute('alt', 'imagem de MaykBrito sorrindo, de jaqueta preta e oculos escuros, em um fundo azul')
-
-        }
-        else
-        {
-            img.setAttribute('src', './assets/avatar.png')
-
-        }
-    
-
+  //substituir imagem
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/Avatar_Miguel_Light-Mode_circular.png");
+    img.setAttribute(
+      "alt",
+      "imagem de miguel tirando foto no espelho de camisa branca "
+    );
+  } else {
+    img.setAttribute("src", "./assets/Avatar_Miguel_Dark-Mode_circular.png");
+  }
 }
